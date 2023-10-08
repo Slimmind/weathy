bc28270e82ab0a1ebfcdbc94053e02debc535102
Pavlo Siroshtan <pavlo_siroshtan@epam.com>
2023-10-08 18:31:02 +0300

    added service-worker

--- src/components/background-section/BackgroundSection.tsx
+++ src/components/background-section/BackgroundSection.tsx
@@ -4,30 +4,30 @@ import { getBackground } from '../../utils/get-background';
 import './background-section.styles.css';
 
 interface BackgroundSectionProps {
-	iconCode: number;
+  iconCode: number;
 }
 
 export const BackgroundSection: React.FC<BackgroundSectionProps> = React.memo(
-	({ iconCode }) => {
-		const [isImageLoaded, setIsImageLoaded] = useState(false);
-		const imageUrl = getBackground(iconCode, Date.now());
+  ({ iconCode }) => {
+    const [isImageLoaded, setIsImageLoaded] = useState(false);
+    const imageUrl = getBackground(iconCode, Date.now());
 
-		const handleImageLoad = (): void => {
-			setIsImageLoaded(true);
-		};
+    const handleImageLoad = (): void => {
+      setIsImageLoaded(true);
+    };
 
-		return (
-			<section
-				className={`background-section ${isImageLoaded ? 'loaded' : ''}`}
-			>
-				{imageUrl && (
-					<img
-						onLoad={handleImageLoad}
-						src={imageUrl}
-						alt='weather background'
-					/>
-				)}
-			</section>
-		);
-	}
+    return (
+      <section
+        className={`background-section ${isImageLoaded ? 'loaded' : ''}`}
+      >
+        {imageUrl && (
+          <img
+            onLoad={handleImageLoad}
+            src={imageUrl}
+            alt="weather background"
+          />
+        )}
+      </section>
+    );
+  }
 );
