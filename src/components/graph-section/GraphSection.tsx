f4db8f62d4e915772b13d37b53f43fec7044ea9b
slim <paulsiroshtan@mind-2.local>
2023-11-30 19:54:24 +0200

    used avarage temp-values for temp-graphs, displayed min temp on temp-graph

--- src/components/graph-section/GraphSection.tsx
+++ src/components/graph-section/GraphSection.tsx
@@ -18,17 +18,19 @@ export const TemperatureGraph: React.FC<TemperatureGraphProps> = ({ data }) => {
 		timestamp: time * 1000,
 		iconCode: data.weathercode[index],
 		maxTemp: Math.round(data.temperature_2m_max[index]),
+		minTemp: Math.round(data.temperature_2m_min[index]),
 	}));
 	const barHeights = getBarHeight(weather);
 
 	return (
 		<section className='temperature-graph'>
 			<ul className='temperature-graph__bars'>
-				{weather.map(({ maxTemp, iconCode, timestamp }, idx) => (
+				{weather.map(({ maxTemp, minTemp, iconCode, timestamp }, idx) => (
 					<GraphBar
 						key={timestamp}
 						time={timestamp}
-						temp={maxTemp}
+						maxTemp={maxTemp}
+						minTemp={minTemp}
 						icon={iconCode}
 						size={barHeights[idx]}
 					/>
--- src/components/graph-section/graph-bar/GraphBar.tsx
+++ src/components/graph-section/graph-bar/GraphBar.tsx
@@ -4,28 +4,35 @@ import { getIcon } from '../../../utils/get-icon';
 import './graph-bar.styles.css';
 
 interface GraphBarProps {
-  time: number;
-  temp: number;
-  icon: number;
-  size: string;
+	time: number;
+	maxTemp: number;
+	minTemp: number;
+	icon: number;
+	size: string;
 }
 
 export const GraphBar: React.FC<GraphBarProps> = ({
-  time,
-  temp,
-  icon,
-  size,
+	time,
+	maxTemp,
+	minTemp,
+	icon,
+	size,
 }) => {
-  const IconComponent = getIcon(icon);
-  const linkUrl = `#${DAY_FORMATTER.format(time).toLowerCase()}`;
-  return (
-    <li className="graph-bar" style={{ height: size }}>
-      <a href={linkUrl} className="graph-bar__link">
-        <strong className="graph-bar_value">{temp}&deg;</strong>
-        <div className="graph-bar__icon">
-          <IconComponent />
-        </div>
-      </a>
-    </li>
-  );
+	const IconComponent = getIcon(icon);
+	const linkUrl = `#${DAY_FORMATTER.format(time).toLowerCase()}`;
+	return (
+		<li className='graph-bar' style={{ height: size }} data-min-temp={minTemp}>
+			<a href={linkUrl} className='graph-bar__link'>
+				<strong className='graph-bar__value graph-bar__value--max'>
+					{maxTemp}&deg;
+				</strong>
+				<strong className='graph-bar__value graph-bar__value--min'>
+					{minTemp}&deg;
+				</strong>
+				<div className='graph-bar__icon'>
+					<IconComponent />
+				</div>
+			</a>
+		</li>
+	);
 };
