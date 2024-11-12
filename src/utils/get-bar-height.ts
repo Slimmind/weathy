01007d60686d576e84cbc99cddeab161c61c49e7
SLIMMIND <slimmind@gmail.com>
2024-11-12 19:35:23 +0300

    fixed graph-bar height, updated forecast styles, added scroll-to-top

--- src/utils/get-bar-height.ts
+++ src/utils/get-bar-height.ts
@@ -9,12 +9,15 @@ export function getBarHeight(data: BarData[]): string[] {
 	const tempValues = data.map((day) => (day.minTemp + day.maxTemp) / 2);
 	const maxTemp = Math.max(...tempValues);
 	const minTemp = Math.min(...tempValues);
-	// const total = Math.abs(maxTemp) + Math.abs(minTemp);
-	const degHeight = 70 / maxTemp;
+	// Учтем диапазон температур, чтобы правильно нормализовать значения
+	const tempRange = maxTemp - minTemp;
+	// Переменная degHeight теперь будет учитывать как максимальное, так и минимальное значения температур
+	const degHeight = 70 / tempRange;
 	const minHeight = 30;
 
+	// Обновим функцию для расчета высоты столбца
 	const barHeight = (temp: number): number => {
-		return minTemp < 0 && temp > minTemp ? temp + Math.abs(minTemp) : temp;
+		return temp - minTemp;
 	};
 
 	return tempValues.map((day) => {
