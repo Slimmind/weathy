273d8c59a49b09fe7ec32ecf4a57f4c5e5fa9ae9
SLIMMIND <slimmind@gmail.com>
2025-02-13 13:14:07 +0300

    updated forecast component (16 values)

--- src/components/forecast/Forecast.tsx
+++ src/components/forecast/Forecast.tsx
@@ -1,59 +1,51 @@
 import React from 'react';
 import { groupForecastValues } from '../../utils/group-forecast-values';
-import { getDateInfo } from '../../utils/get-date-info';
-import { DayInfo } from './day-info/DayInfo';
 import './forecast.styles.css';
 
 type ForecastProps = {
-  data: any;
+	data: any;
 };
 
 export const Forecast = ({ data }: ForecastProps) => {
-  const tempData = groupForecastValues(data.hourly.temperature_2m);
-  const timeData = data.hourly.time;
-  const minTempValue = Math.min(...data.hourly.temperature_2m);
-  const maxTempValue = Math.max(...data.hourly.temperature_2m);
-  const tempRange = maxTempValue - minTempValue;
-  const minGraphHeight = 15;
+	const tempData = groupForecastValues(data.hourly);
+	const averageDayTemp = tempData.map(({ min, max }) =>
+		min + max < 0 ? Math.floor((min + max) / 2) : Math.ceil((min + max) / 2)
+	);
+	const minTempValue = Math.min(...data.hourly.temperature_2m);
+	const maxTempValue = Math.max(...data.hourly.temperature_2m);
+	const tempRange = maxTempValue - minTempValue;
+	const minGraphHeight = 25;
 
-  const setGraphHeight = (temp: number): string => {
-    const shiftedTemp = temp - minTempValue;
-    const height = (85 / tempRange) * shiftedTemp + minGraphHeight;
-    return `${Math.min(height, 100)}%`;
-  };
+	const setGraphHeight = (temp: number): string => {
+		const shiftedTemp = temp - minTempValue;
+		const height = (75 / tempRange) * shiftedTemp + minGraphHeight;
+		return `${Math.min(height, 100)}%`;
+	};
 
-  const getHour = (value: number): string | number => {
-    const hour = value + 1;
-    return hour < 10 ? `0${hour}` : hour;
-  };
-
-  const timeString = (idx: number): string => timeData[idx * 24];
-
-  return (
-    <div className='forecast'>
-      <div className='forecast__wrapper'>
-        {tempData.map((day, idx) => (
-          <div key={timeString(idx)} className='forecast__graph-wrapper'>
-            <DayInfo
-              dateData={getDateInfo(timeString(idx))}
-              minTemp={day.min}
-              maxTemp={day.max}
-            />
-            <ul className='forecast__graph'>
-              {day.values.map((value, idx) => (
-                <li
-                  key={idx}
-                  className='forecast__graph-item'
-                  style={{ height: setGraphHeight(value) }}
-                >
-                  <span className='forecast__temperature'>{value}&deg;</span>
-                  <span className='forecast__hour'>{getHour(idx)}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className='forecast'>
+			<div className='forecast__wrapper'>
+				<div className='forecast__graph-wrapper'>
+					<div className='forecast__day-info'>
+						<span>Temperature forecast for 16 days</span>
+					</div>
+					<ul className='forecast__graph'>
+						{averageDayTemp.map((day, idx) => (
+							<li
+								key={idx}
+								className='forecast__graph-item'
+								style={{ height: setGraphHeight(day) }}
+							>
+								<span className='forecast__temperature'>{day}&deg;</span>
+								<div className='forecast__date'>
+									<span>{tempData[idx].date.dayName}</span>
+									<span>{tempData[idx].date.dayNum}</span>
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
